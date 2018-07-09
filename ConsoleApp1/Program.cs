86cee65621826e4d78b1f075e02e0a81f4e19e43
wordsonice <3258430464@qq.com>
2018-07-09 13:03:44 +0800

    study

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Output text to the screen.
+            Console.WriteLine(value: "The first app in Beginning Visual C# 2017!");
+            Console.ReadKey();
+
+        }
+    }
+}
